5b2b7a67d82907467fdd976d5a37a5e359f90262
moayyad saleh <137034202+moayyadsaleh@users.noreply.github.com>
2023-06-29 10:00:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,226 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Harmony Heaven Hotel</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Harmony<span>Haven</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Projects</a></li>
+                <li><a href="#contact" class="menu-btn">Blog</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+               
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Welcome to</div>
+                <div class="text-2">Harmony Haven Hotel</div>
+                <div class="text-3">Experience the perfect blend of <span class="typing"></span></div>
+                <a href="#">Book Now</a>
+            </div>
+        </div>
+    </section>
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Elegance", "tranquility", "indulgence", "hospitality", "luxury "],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About Harmony Haven Hotel</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="https://media.architecturaldigest.com/photos/5e3b499fc63e7d0009ada0f7/16:9/w_2560%2Cc_limit/Exte%25CC%2581rieur%2520-%2520Fac%25CC%25A7ade-2%2520jpg.jpg" alt="Harmony Haven Hotel">
+                </div>
+                <div class="column right">
+                    <div class="text">Welcome to Harmony Haven Hotel, where tranquility meets luxury.</div>
+                    <p>Experience a serene and unforgettable stay at our boutique hotel. Nestled amidst breathtaking nature, we offer a sanctuary of comfort and relaxation. Our dedicated team is committed to providing exceptional service and ensuring your every need is met. Discover our elegantly designed rooms, indulge in delectable cuisine at our renowned restaurant, and rejuvenate your senses at our spa and wellness center.</p>
+                    <a href="index.html">Book Your Stay</a>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">Our Services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-utensils"></i>
+                        <div class="text">Dining Experience</div>
+                        <p>Indulge in a culinary journey with our exquisite dining options. Our talented chefs craft delectable dishes using the finest ingredients, ensuring a delightful gastronomic experience.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-spa"></i>
+                        <div class="text">Spa & Wellness</div>
+                        <p>Relax and rejuvenate your mind, body, and soul at our state-of-the-art spa and wellness center. Immerse yourself in a range of soothing treatments and therapies designed to enhance your well-being.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-swimming-pool"></i>
+                        <div class="text">Pool & Recreation</div>
+                        <p>Unwind by our sparkling pool, surrounded by lush landscapes. Enjoy a refreshing swim or bask in the sun while sipping on your favorite beverage. For the adventurous, explore nearby attractions and outdoor activities.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
+    <!-- Expertise section start -->
+<section class="skills" id="skills">
+    <div class="max-width">
+        <h2 class="title">Discover Our Exceptional Offerings</h2>
+        <div class="skills-content">
+            <div class="column left">
+                <div class="text"></div>
+                <p>Experience the pinnacle of luxury at Harmony Haven Hotel, where our dedicated team of seasoned professionals is committed to crafting extraordinary moments that will leave a lasting impression on our esteemed guests. With an extensive background in the art of hospitality, we take pride in providing unparalleled service that goes above and beyond expectations.</p>
+                <a href="#">Explore Further</a>
+            </div>
+            <div class="column right">
+                <div class="bars">
+                    <div class="info">
+                        <span>Luxurious Accommodations</span>
+                        <span>100%</span>
+                    </div>
+                    <div class="line html"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Exquisite Dining Experiences</span>
+                        <span>100%</span>
+                    </div>
+                    <div class="line css"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Serenity and Wellness Retreats</span>
+                        <span>100%</span>
+                    </div>
+                    <div class="line js"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Personalized Concierge Services</span>
+                        <span>100%</span>
+                    </div>
+                    <div class="line php"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Impeccable Event Planning</span>
+                        <span>100%</span>
+                    </div>
+                    <div class="line mysql"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section>
+<div id="seperator-image"> 
+    <img src="https://ak-d.tripcdn.com/images/0225x120009sybj7z0DE8_R_960_660_R5_D.jpg" style="width: 100%; max-height: 100vh; margin-top: 10vh; margin-bottom: 10vh;" alt="hotel photo">
+</div>
+</section>
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Get in touch</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Contact Information</div>
+                    <p>Feel free to reach out to us for any inquiries or reservations. We're here to assist you!</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Hotel Name</div>
+                                <div class="sub-title">Harmony Haven Hotel</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">123 Main Street, City, Country</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">info@harmonyhavenhotel.com</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-phone"></i>
+                            <div class="info">
+                                <div class="head">Phone</div>
+                                <div class="sub-title">+1 123 456 7890</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Send us a message</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Your Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Your Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Your Message..." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="index.html"> Harmony Heaven Hotel</a> | <span class="far fa-copyright"></span> 2023 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
